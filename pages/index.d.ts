28f99d67e525eef66a4da17a61a79cb063f27b97
lautom36 <lautom36@gmail.com>
2022-07-14 14:14:13 -0600

    basic ui added

---
+++ pages/index.d.ts
@@ -0,0 +1,4 @@
+import React, { ReactElement } from "react"
+
+export type IndexProps = {
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,49 @@
 import type { NextPage } from 'next'
-import styles from 'styles/Home.module.css'
+import styles from './index.module.css'
+import { Button } from 'components/Button/Button'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
+  const [file, setFile] = useState(null);
+
+  function handleFileDrop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    setFile(e.dataTransfer.items[0].getAsFile())
+  }
+
+  function handleBrowse(e) {
+    console.log(e);
+    setFile(e.target.files[0]);
+  }
+
   return (
     <div className={styles.container}>
-      Home Page
+      <div className={styles.wrapper}>
+        <div>
+          <p className={styles.title}>
+            DCC Verifier+
+          </p>
+        </div>
+        <div>
+          <p className={styles.description}>
+            This is an explanation of what this site is and what it does. This is an explanation of what this site is and what it does. This is an explanation of what this site is and what it does.
+          </p>
+        </div>
+        <Button text='Scan QR Code' />
+        <div
+          className={styles.dndUpload}
+          onDrop={handleFileDrop}
+          onDragOver={(e) => { e.preventDefault(); }}
+          >
+            Drag and drop a file here or <label className={styles.fileUpload}>
+              <input type='file' onChange={handleBrowse} />
+              <span className={styles.browseLink}>browse</span>
+            </label>
+            <h6>Supports JSON</h6>
+        </div>
+        <textarea className={styles.textarea} placeholder='Past JSON or URL' />
+      </div>
     </div>
   )
 }
