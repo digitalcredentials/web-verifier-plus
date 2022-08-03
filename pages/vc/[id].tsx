79895c6ef019c161d9e0c31121ac66163db6c7ec
lautom36 <72038363+lautom36@users.noreply.github.com>
2022-08-03 14:59:42 -0600

    Tl/setup index UI (#9)
    * created and added the top bar
    
    * added bottom bar and added resizing
    
    * fixed mobile compatability
    
    * fixed top and bottom bar for id page
    
    * fixed merge conflict and fixed typo
    
    * one last merge conflict
    
    * updated ui and hooked up verify button
    
    * updated ui to match designs
    
    * fixed issue on [id] page, removed login button
    
    * fixed darkmode toggle not staying toggled on reload
    
    * think somthing wrong happened in the merge
    
    Co-authored-by: lautom36 <lautom36@gmail.com>

--- pages/vc/[id].tsx
+++ pages/vc/[id].tsx
@@ -12,17 +12,21 @@ import { TopBar } from 'components/TopBar/TopBar';
 import { BottomBar } from 'components/BottomBar/BottomBar';
 
 const CredentialPage: NextPage<CredentialProps> = ({ presentation }) => {
-  const credentialContext = useVerification(presentation.verifiableCredential as Credential);
   const [isDark, setIsDark] = useState(false);
+  const credentialContext = useVerification(presentation.verifiableCredential as Credential);
   return (
-    <VerificationContext.Provider value={credentialContext}>
-      <Container>
-        <TopBar hasLogo isDark={isDark} setIsDark={setIsDark}/>
-        <CredentialCard presentation={presentation} />
-        <VerificationCard />
-        <BottomBar isDark={isDark} />
-      </Container>
-    </VerificationContext.Provider>
+    <div className={styles.container}>
+      <TopBar hasLogo={true} isDark={isDark} setIsDark={setIsDark} />
+      <div className={styles.verifyContainer}>
+        <VerificationContext.Provider value={credentialContext}>
+          <Container>
+            <CredentialCard presentation={presentation} />
+            <VerificationCard />
+          </Container>
+        </VerificationContext.Provider>
+      </div>
+      <BottomBar isDark={isDark}/>
+    </div>
   )
 }
 
