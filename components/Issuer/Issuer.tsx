8d729bbac092465b9defd1a66e05c728f2353853
lautom36 <lautom36@gmail.com>
2022-08-24 13:18:37 -0600

    fixed 404 error

--- components/Issuer/Issuer.tsx
+++ components/Issuer/Issuer.tsx
@@ -1,18 +1,22 @@
+import { useEffect, useRef } from 'react';
 import type { IssuerProps } from './Issuer.d';
 import styles from './Issuer.module.css';
-export const Issuer = ({issuer, header}: IssuerProps ) => {
 
-  const img: HTMLElement = document.getElementById("IssuerImage")!;
-  console.log(img);
+export const Issuer = ({issuer, header}: IssuerProps ) => {
+  let issuerImage = useRef<HTMLImageElement>(null);
+
   const handleonError = () => {
-    img.style.visibility = 'hidden';
+    if ( issuerImage.current != null) {
+      issuerImage.current.style.visibility = 'hidden';
+    }
   }
+
   return (
     <div>
       <h3 className={styles.header}>{header}</h3>
       <div className={styles.issuer}>
         {issuer.image && (
-          <img src={issuer.image} width={36} height={36} alt="Issuer Image" id='IssuerImage' onError={handleonError} />
+          <img src={issuer.image} width={36} height={36} alt="Issuer Image" ref={issuerImage} onError={handleonError} />
         )}
         <div className={styles.issuerInformation}>
           <div>{issuer.name}</div>
