c518d74b0ec89c928268f0fbe54a1160e680b592
lautom36 <72038363+lautom36@users.noreply.github.com>
2022-09-19 13:56:35 -0600

    Tl/fixing bugs (#20)
    * fixed merge issue
    
    * added version number
    
    * fixed label error with wave
    
    * fixed version styles
    
    * push for vercel update
    
    * removed duplicate
    
    * added version number
    
    * fixed label error with wave
    
    * fixed version styles
    
    * push for vercel update
    
    * added links to static html
    
    * moved static files to public folder
    
    * was causing an issue with vercel
    
    Co-authored-by: lautom36 <lautom36@gmail.com>

--- components/BottomBar/BottomBar.tsx
+++ components/BottomBar/BottomBar.tsx
@@ -14,12 +14,12 @@ export const BottomBar = ({isDark}: BottomBarProps) => {
         </a>
       </div>
       <div className={styles.linkContainer}>
-        <a className={styles.link}>Terms and Conditions of Use</a>
-        <a className={styles.link}>Privacy Policy</a>
+        <a href='/html/terms.html' className={styles.link}>Terms and Conditions of Use</a>
+        <a href='/html/privacy.html' className={styles.link}>Privacy Policy</a>
         <a className={styles.link} href='https://accessibility.mit.edu/'>Accessibility</a>
         <a className={styles.link}>View on Github</a>
       </div>
-      <p className={styles.version}>VerifierPlus Version 1.0.1</p>
+      <p className={styles.version}>VerifierPlus Version 1.0.2</p>
     </div>
   )
 }
