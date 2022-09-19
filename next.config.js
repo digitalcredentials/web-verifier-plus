686261ba4954ca6abf1683d24344ab93dd6d6e73
lautom36 <lautom36@gmail.com>
2022-09-19 11:49:18 -0600

    added links to static html

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  rewrites: async () => {
+    return [
+      {
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
