6275b018ce4f6a1940276efef96060754b24a0f8
lautom36 <thomasjlau36@gmail.com>
2022-09-19 11:59:56 -0600

    was causing an issue with vercel

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  rewrites: async () => {
-    return [
-      {
-      }
-    ]
-  }
 }
 
 module.exports = nextConfig
