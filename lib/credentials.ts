2b7c570af990e026fc99b13206094d828a300739
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-12-04 15:05:35 -0500

    Implement soft delete / unshare.

--- lib/credentials.ts
+++ lib/credentials.ts
@@ -3,7 +3,7 @@ import { dbCredentials } from './database';
 import { VerifiablePresentation } from '../types/presentation';
 import { VerifiableCredential } from '../types/credential';
 
-export type StoreCredentialPayload = {
+export type CredentialPayload = {
   // a signed VP containing one VC (more than one will be supported in the future)
   vp: VerifiablePresentation;
 }
@@ -15,6 +15,8 @@ export type GetCredentialResult = {
   controller: string;
   // VC extracted from the VP for client-side convenience
   credential: VerifiableCredential;
+  // 'shared' is used for "soft delete" -- if set to 'false', the VC is deleted/unshared
+  shared: boolean;
 }
 
 /**
@@ -25,7 +27,7 @@ export type GetCredentialResult = {
  *
  * @returns {Promise<string>} Relative URL at which the credential was stored.
  */
-export async function post({ vp }: StoreCredentialPayload): Promise<string> {
+export async function post({ vp }: CredentialPayload): Promise<string> {
   const { holder } = vp;
   if(!holder) {
     throw new Error("Missing 'holder' property. DID Authentication is required.");
@@ -33,15 +35,20 @@ export async function post({ vp }: StoreCredentialPayload): Promise<string> {
 
   const Credentials = await dbCredentials.open();
   const credential = _extractCredential(vp);
-  const id = publicIdFrom(holder, credential);
+  const publicId = publicIdFrom(holder, credential);
+
+  // TODO: Set automatic expiration date
+  // @see https://www.mongodb.com/docs/v6.0/tutorial/expire-data/
+
   await Credentials.insert({
-    id,
+    id: publicId,
     controller: holder,
-    vp
+    vp,
+    shared: true
   });
   await dbCredentials.close();
 
-  return `/credentials/${id}`
+  return `/credentials/${publicId}`
 }
 
 export function publicIdFrom (holder: string, credential: VerifiableCredential): string {
@@ -59,7 +66,7 @@ function _extractCredential(vp: VerifiablePresentation): VerifiableCredential {
 /**
  * Loads a credential from database by id.
  *
- * @param credentialId {VerifiablePresentation}
+ * @param credentialId {string}
  *
  * @returns {Promise<GetCredentialResult>}
  */
@@ -68,7 +75,51 @@ export async function get({ publicCredentialId }: any): Promise<GetCredentialRes
   const result = await Credentials.findOne({ id: publicCredentialId });
   await dbCredentials.close();
 
+  if(!result || !result.shared) {
+    const notFound: any = new Error('Public share of this credential expired, unshared, or not found.')
+    notFound.statusCode = 404;
+    notFound.statusText = 'Not found'
+    throw notFound;
+  }
+
   const credential = _extractCredential(result.vp);
 
   return { credential, ...result }
+}
+
+/**
+ * Unshares a credential from database by id (performs a "soft delete").
+ *
+ * @param publicCredentialId {string}
+ * @param payload {CredentialPayload} - { vp: signedVerifiablePresentation }
+ *
+ * @returns {Promise<GetCredentialResult>}
+ */
+export async function unshare({ publicCredentialId, payload }: any): Promise<GetCredentialResult> {
+  const Credentials = await dbCredentials.open();
+  const result = await Credentials.findOne({ id: publicCredentialId });
+
+  if(!result) {
+    const notFound: any = new Error('This credential does not exist, has expired, or has already been unshared.')
+    notFound.statusCode = 404;
+    notFound.statusText = 'Not found'
+    await dbCredentials.close();
+    throw notFound;
+  }
+
+  const filter = { id: publicCredentialId };
+  const options = { upsert: false }
+  const update = {
+    $set: {
+      shared: false
+    }
+  }
+
+  const unshareResult: any = await Credentials.updateOne(filter, update, options);
+
+  console.log('VC unshare result:', JSON.stringify(unshareResult, null, 2));
+
+  await dbCredentials.close();
+
+  return { ...unshareResult }
 }
--- pages/api/credentials/[publicCredentialId].ts
+++ pages/api/credentials/[publicCredentialId].ts
@@ -3,28 +3,41 @@ import * as credentials from 'lib/credentials';
 
 /**
  * GET /api/credentials/{publicCredentialId}
+ * DELETE /api/credentials/{publicCredentialId}
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log('started api');
   try {
+    let result: any;
+    const { publicCredentialId } = req.query;
     switch (req.method) {
       case 'GET':
-        const { publicCredentialId } = req.query;
         console.log(`GET /credentials/${publicCredentialId}`)
 
-        const result: any = await credentials.get({ publicCredentialId });
+        result = await credentials.get({ publicCredentialId });
         console.log('Loaded credential:', result.credential);
 
         res.status(200).json(result);
         break;
+      case 'DELETE':
+        console.log(`DELETE /credentials/${publicCredentialId}`)
+
+        result = await credentials.unshare({ publicCredentialId, payload: req.body });
+        console.log('Unshared credential.');
+
+        res.status(200).json({ message: 'Credential unshared.' });
+        break;
       default:
-        res.setHeader('Allow', 'GET');
+        res.setHeader('Allow', 'GET, DELETE');
         res.status(405).json({ status: 'Method not allowed' })
     }
-  } catch (error) {
+  } catch (error: any) {
     console.error(error);
-    res.status(400).json({
-      status: 'Invalid request',
+
+    const statusCode = error.statusCode || 400;
+
+    res.status(statusCode).json({
+      status: error.statusText || 'Invalid request',
       // @ts-ignore
       error: error.message
     })
