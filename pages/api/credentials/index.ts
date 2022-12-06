9596a17019b234da839a0498ec902f93b5a6ff8a
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-12-06 16:12:12 -0500

    Expand return format for storing credential.

--- pages/api/credentials/index.ts
+++ pages/api/credentials/index.ts
@@ -4,6 +4,31 @@ import * as credentials from 'lib/credentials';
 /**
  * POST /api/credentials
  * Adds a new credential to the database.
+ *
+ * Request:
+ * - `Content-type: application/json` header is required (otherwise, the JSON
+ *   body parser for Next.js API routes won't activate).
+ *
+ * - Body is of the format:
+ * {
+ *   vp: signed verifiable presentation goes here
+ * }
+ *
+ * Response (content-type: application/json):
+ * {
+ *   "url": {
+ *     // human-readable HTML view of the credential
+ *     "view": "/credentials/{publicCredentialId}",
+ *
+ *     // raw JSON GET (used by the html view)
+ *     "get": "/api/credentials/{publicCredentialId}",
+ *
+ *     // used for DELETE/unshare API
+ *     "unshare": "/api/credentials/{publicCredentialId}"
+ *   }
+ * }
+ *
+ * (Note the /api/ prefix in the get and unshare URLs, above).
  */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
@@ -11,11 +36,11 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       case 'POST':
         console.log('POST /credentials:', req.body)
 
-        const url = await credentials.post(req.body);
-        console.log('Credential stored at:', url);
+        const result = await credentials.post(req.body);
+        console.log('Credential stored:', JSON.stringify(result, null, 2));
 
-        res.setHeader('Location', url);
-        res.status(201).json({ status: 'Credential added', location: url });
+        res.setHeader('Location', result.url.view);
+        res.status(201).json({ status: 'Credential added', ...result });
         break;
       default:
         res.setHeader('Allow', 'POST');
