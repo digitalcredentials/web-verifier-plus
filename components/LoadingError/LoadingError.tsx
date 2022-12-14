1ace92a5b209abf6d7fda7f5f350eb9435d580f0
lautom36 <thomasjlau36@gmail.com>
2022-12-14 13:24:27 -0700

    made beta text bigger

--- components/LoadingError/LoadingError.tsx
+++ components/LoadingError/LoadingError.tsx
@@ -5,7 +5,8 @@ export const LoadingError = () => {
   return(
     <div className={styles.loadingErrorContainer}>
       <h1 className={styles.errorCode}>404 credential not found</h1>
-      <h3 className={styles.message}>Double check that you used the right public id for your credential</h3>
+      <h3 className={styles.message}>Please check to make sure you’re visiting the correct link.</h3>
+      <h3 className={styles.message}>You may also want to check with the credential holder to see if they have unshared the credential or it has expired.</h3>
     </div>
   )
 }
