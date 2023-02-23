838cd0023cf2cda0eda033d5e5427596021bbd61
James Chartrand <jc.chartrand@gmail.com>
2023-02-23 17:52:17 -0500

    include OBv3 fields in credential display

--- components/CredentialCard/CredentialCard.d.ts
+++ components/CredentialCard/CredentialCard.d.ts
@@ -1,7 +1,15 @@
-import type { Credential } from 'types/credential';
-import { VerifiablePresentation } from 'types/presentation';
+import type { VerifiableCredential } from 'types/credential';
 
 export type CredentialCardProps = {
-  credential: Credential,
+  credential: VerifiableCredential,
   wasMulti?: Boolean
+}
+
+export type CredentialDisplayFields = {
+  credentialName: string | undefined,
+  issuedTo: string | undefined,
+  issuanceDate: string | undefined,
+  expirationDate: string | undefined,
+  credentialDescription: string | undefined,
+  criteria: string | undefined,
 }
