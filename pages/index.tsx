b4cfc729a7f4fc17ac7600a171c277e00b101f58
Alex H <ahiguera@Alexs-MacBook-Pro.local>
2024-02-14 11:30:39 -0500

    Remove Beta from main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -180,7 +180,7 @@ const Home: NextPage = () => {
       <div className={styles.contentContainer}>
         <div>
           <h1 className={styles.title}>
-            VerifierPlus <span className={styles.betaTag}>Beta</span>
+            VerifierPlus
           </h1>
         </div>
         <div>
