7d10cd55544c2de0f2cef8baa2f27054c54d755a
Santhosh Vijay <santosh.v@oneorigin.us>
2025-04-01 18:28:54 -0700

    Updated method presentation Signed-off-by: Santhosh Vijay <santosh.v@oneorigin.us>

--- lib/validate.ts
+++ lib/validate.ts
@@ -1,14 +1,7 @@
-import { Ed25519Signature2020 } from '@digitalcredentials/ed25519-signature-2020';
-import { purposes } from 'jsonld-signatures';
 import * as verifierCore from '@digitalcredentials/verifier-core';
 import { VerifiablePresentation, PresentationError } from 'types/presentation.d';
 import { VerifiableCredential, CredentialError, CredentialErrorTypes } from 'types/credential.d';
-import { securityLoader } from '@digitalcredentials/security-document-loader';
 import { KnownDidRegistries } from './../data/knownRegistries'
-
-const documentLoader = securityLoader({ fetchRemoteContexts: true }).build()
-const suite = new Ed25519Signature2020();
-const presentationPurpose = new purposes.AssertionProofPurpose();
 
 export type ResultLog = {
   id: string,
@@ -35,12 +28,8 @@ export async function verifyPresentation(
   unsignedPresentation = true,
 ): Promise<VerifyResponse> {
   try {
-    const result = await verifierCore.verify({
-      presentation,
-      presentationPurpose,
-      suite,
-      documentLoader,
-      unsignedPresentation,
+    const result = await verifierCore.verifyPresentation({
+      presentation
     });
 
     return result;
