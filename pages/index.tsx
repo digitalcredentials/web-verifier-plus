13d9e37e9745944113c35174ce4c5c1fa3126d7f
Alex H <ahiguera@mit.edu>
2025-04-10 12:42:46 -0400

    Add rel={'noreferrer'}

--- pages/index.tsx
+++ pages/index.tsx
@@ -354,7 +354,7 @@ const Home: NextPage = () => {
             onClose={() => stopPolling(undefined)}
             title="Request credentials from LCW" >
             <p>
-              <a className={styles.lcwLink} target={'_blank'} href={lcwRequestUrl}><h3>Mobile Link</h3></a>
+              <a className={styles.lcwLink} target={'_blank'} rel={'noreferrer'} href={lcwRequestUrl}><h3>Mobile Link</h3></a>
             </p>
             <div><h5 className={styles.lcwLink}>Open Request in wallet via QR Code:</h5></div>
             <div className={styles.qrCode}>
